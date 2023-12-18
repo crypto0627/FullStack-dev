3e75cbe8fe947d16d3a1bf5d8ebc20b796317d88
crypto0627 <jake0627a1@gmail.com>
2023-12-18 21:58:41 +0800

    fix: integrate turbo and nextjs config

--- apps/express-backend/src/untils/bcryptUtils.ts
+++
@@ -1,8 +0,0 @@
-// src/utils/bcryptUtils.ts
-
-import bcrypt from 'bcryptjs';
-
-export const bcryptUtils = {
-  hashPassword: (password: string): Promise<string> => bcrypt.hash(password, 10),
-  comparePassword: (password: string, hashedPassword: string): Promise<boolean> => bcrypt.compare(password, hashedPassword),
-};
